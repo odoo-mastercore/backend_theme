201b7ddde390b228555f2f66c13f5a2e587f16c8
FA <FA>
2020-04-20 21:19:45 -0300

    [IMP] Initial Commit

---
+++ backend_theme/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 MASTERCORE
+# Thanks Openwork Material Backend Theme V13
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "MASTERCORE Simplified Material Backend Theme",
+    "summary": "MASTERCORE Simplified Material Backend Theme",
+    "version": "13.0.0.1",
+    "category": "Theme/Backend",
+    "website": "https://mastercore.net",
+	"description": """
+		MASTERCORE Simplified Material Backend theme for Odoo 13.0 CE
+        Thanks to Openwork Material Backend Theme V13
+    """,
+	'images':[
+        'images/screen.png'
+	],
+    "author": "MASTERCORE || SINAPSYS Global",
+    "license": "LGPL-3",
+    "installable": True,
+    "depends": [
+        'web',
+        'web_responsive',
+
+    ],
+    "data": [
+        'views/assets.xml',
+		'views/res_company_view.xml',
+    ],
+}
+
