7aa168f7a9d7a37ad138df36eef6bb32370e13b1
rhe-mastercore <rhe@mastercore.net>
2020-05-12 10:31:03 -0400

    [ADD] included screenshot.

--- backend_theme/__manifest__.py
+++ backend_theme/__manifest__.py
@@ -4,19 +4,19 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
-    "name": "MASTERCORE Simplified Material Backend Theme",
-    "summary": "MASTERCORE Simplified Material Backend Theme",
+    "name": "Simplified Enterprise Backend Theme",
+    "summary": "Simplified Enterprise Backend Theme",
     "version": "13.0.0.1",
     "category": "Theme/Backend",
     "website": "https://mastercore.net",
 	"description": """
-		MASTERCORE Simplified Material Backend theme for Odoo 13.0 CE
+		Simplified Enterprise Backend Theme for Odoo 13.0 CE
         Thanks to Openwork Material Backend Theme V13
     """,
 	'images':[
         'images/screen.png'
 	],
-    "author": "MASTERCORE SAS|| SINAPSYS GLOBAL SA",
+    "author": "MASTERCORE SAS || SINAPSYS GLOBAL SA",
     "license": "LGPL-3",
     "installable": True,
     "depends": [
