1ebb42e10326e58c5dcb39395ab13451e0a9aae2
FAR <far@mastercore.net>
2020-06-10 15:06:28 -0300

    feat: Simplified simulated EE Backend Theme

--- backend_theme/__manifest__.py
+++ backend_theme/__manifest__.py
@@ -4,13 +4,13 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
-    "name": "Simplified simulated EE Backend Theme",
-    "summary": "Simplified simulated EE Backend Theme",
+    "name": "Simplified emulated EE Backend Theme",
+    "summary": "Simplified emulated EE Backend Theme",
     "version": "13.0.0.1",
     "category": "Theme/Backend",
     "website": "https://mastercore.net",
 	"description": """
-		Simplified simulated EE Backend Theme for Odoo 13.0 CE
+		Simplified emulated EE Backend Theme for Odoo 13.0 CE
         Thanks to Openwork Material Backend Theme V13
     """,
     "author": "MASTERCORE SAS || SINAPSYS GLOBAL SA",
