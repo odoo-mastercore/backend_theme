59d6788e7b158de00fcf29db0d6441ce4016389d
Far <far@mastercore.net>
2021-01-11 02:05:56 -0300

    [ADD] Version 14.0 simplified

--- backend_theme/__manifest__.py
+++ backend_theme/__manifest__.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
-# Copyright 2020 MASTERCORE
-# Thanks Openwork Material Backend Theme V13
+# Copyright 2020-2021 MASTERCORE
+# Special Thanks to Mario Gielissen Openwork Material Backend Theme V14
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Simplified emulated EE Backend Theme",
     "summary": "Simplified emulated EE Backend Theme",
-    "version": "13.0.0.1",
+    "version": "14.0.1.0",
     "category": "Theme/Backend",
-    "website": "https://mastercore.net",
+    "website": "https://www.openworx.nl",
 	"description": """
-		Simplified emulated EE Backend Theme for Odoo 13.0 CE
-        Thanks to Openwork Material Backend Theme V13
+		Simplified emulated EE Backend Theme for Odoo 14.0 CE
+        Special Thanks to Mario Gielissen Openwork Material Backend Theme V14
     """,
-    "author": "MASTERCORE SAS || SINAPSYS GLOBAL SA",
+	"author": "Openworx, MASTERCORE SAS || SINAPSYS GLOBAL SA",
     "license": "LGPL-3",
     "installable": True,
     "depends": [
@@ -23,6 +23,9 @@
     ],
     "data": [
         'views/assets.xml',
+		'views/res_company_view.xml',
+		'views/users.xml',
+        	'views/sidebar.xml',
     ],
 }
 
